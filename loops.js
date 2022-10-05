0c1d88c97671688780bc9d75919718494494fcb9
Luciefifi <niyomutonilucie@gmail.com>
2022-10-05 16:11:39 +0200

    added loops.js file

---
+++ loops.js
@@ -0,0 +1,16 @@
+var myArray = [];
+var arr = [ "Lucie", "steve"];
+let i =0;
+while(i<=5)
+{
+    myArray.push(i);
+    i++;
+}
+let a = 10;
+while(a>=0)
+{
+    arr.push(a);
+    a--;
+}
+console.log(myArray);
+console.log(arr);
