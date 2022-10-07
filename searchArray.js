188d58fdc663d7f2b6c302a0a0b0a2006df6488c
Luciefifi <niyomutonilucie@gmail.com>
2022-10-07 07:09:05 +0200

    created searchArray.js file

---
+++ searchArray.js
@@ -0,0 +1,16 @@
+function findNum(arr , k)
+{
+    let arra = [];
+    for(let i=0 ; i<arra.length ; i++)
+    {
+        if (arra[i] == k)
+        {
+            console.log("YES");
+        }
+        else{
+            console.log("NO");
+        }
+    }
+}
+let arr1 = [1,2,3,4,5,6,7];
+findNum(arr1 , 6);
